8b941f5b082f48ecc1c9041b669ac57e32e9313f
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-10-08 23:16:38 +0530

    the site is live with minor features

--- secure-cloud-frontend/src/services/api.js
+++ secure-cloud-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // adjust to your backend URL
+  baseURL: "https://securefiletransfer-0kub.onrender.com/api", // adjust to your backend URL
   timeout: 15000,
 });
 
