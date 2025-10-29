15673c2366e294599fb600977f9d25f2a0e8e8c1
kartikeyCS22 <kingkartikey007@gmail.com>
2025-10-29 22:15:50 +0530

    POST CHECK

--- secure-cloud-frontend/src/pages/Home.jsx
+++ secure-cloud-frontend/src/pages/Home.jsx
@@ -146,7 +146,7 @@ const Home = () => {
                 </>
               ) : (
                 <>
-                  <FaEdit /> Edit profile
+                  <FaEdit /> Edit Profile
                 </>
               )}
             </motion.button>
