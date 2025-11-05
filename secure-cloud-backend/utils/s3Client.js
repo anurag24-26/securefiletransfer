5b327c5d9b7d024681593aaae38cc78bc57eb62c
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-11-05 15:39:12 +0530

    download issue resolve

--- secure-cloud-backend/utils/s3Client.js
+++ secure-cloud-backend/utils/s3Client.js
@@ -1,7 +1,8 @@
+// utils/s3Client.js
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-  endpoint: `https://${process.env.B2_ENDPOINT}`,
+  endpoint: process.env.B2_ENDPOINT,  // ✅ no extra https://
   region: "us-east-005",
   signatureVersion: "v4",
   credentials: {
