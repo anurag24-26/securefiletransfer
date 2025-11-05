a100721fb990d2317cca3ca2f7703541b306d659
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-11-05 11:15:26 +0530

    Cloud part Add

--- secure-cloud-backend/utils/s3Client.js
+++ secure-cloud-backend/utils/s3Client.js
@@ -1,8 +1,9 @@
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-  endpoint: process.env.B2_ENDPOINT,
+  endpoint: `https://${process.env.B2_ENDPOINT}`,
   region: "us-east-005",
+  signatureVersion: "v4",
   credentials: {
     accessKeyId: process.env.B2_KEY_ID,
     secretAccessKey: process.env.B2_APP_KEY,
