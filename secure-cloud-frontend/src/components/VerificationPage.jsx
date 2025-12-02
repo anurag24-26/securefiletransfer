9dcb95fe657589761a86e2724aaa51f9943677d2
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-12-02 23:14:17 +0530

    Verification feature made

--- secure-cloud-frontend/src/components/VerificationPage.jsx
+++ secure-cloud-frontend/src/components/VerificationPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import api from "../api"; // your axios instance
+import api from "../services/api"; // your axios instance
 
 const VerificationPage = () => {
   const [file, setFile] = useState(null);
